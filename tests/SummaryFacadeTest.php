f278d48a01f2caf92b5bd6be8c238c32465f1640
cagy <ml.cagy@seznam.cz>
2021-05-23 11:16:24 +0200

    testing environment

---
+++ tests/SummaryFacadeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class SummaryFacadeTest extends TestCase
+{
+
+    public function testBasic() : void
+    {
+        $this->assertEquals(1,1);
+    }
+}
