ce2a64376dd76acca289370d47ce808355a5fd5d
Vojtěch Danišík <32537562+danisik@users.noreply.github.com>
2021-06-07 18:15:28 +0200

    SummaryFacade + SettingsFacade tests

--- tests/model/facade/SettingsFacadeTest.php
+++ tests/model/facade/SettingsFacadeTest.php
@@ -3,23 +3,88 @@
 namespace model\facade;
 
 use App\model\facade\SettingsFacade;
+use App\Model\Facade\SummaryFacade;
+use App\Model\Repository\SettingsRepository;
+use App\Model\Repository\StudentRepository;
+use App\Model\Repository\TaskRepository;
+use App\service\FileService;
+use phpDocumentor\Reflection\Types\This;
 use PHPUnit\Framework\TestCase;
 
 class SettingsFacadeTest extends TestCase
 {
 
+    private ?StudentRepository $studentRepository;
+    private FileService $fileService;
+    private ?TaskRepository $taskRepository;
+    private ?SettingsRepository $settingsRepository;
+    private ?SettingsFacade $settingsFacade;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->studentRepository = $this->createMock(StudentRepository::class);
+        $this->taskRepository = $this->createMock(TaskRepository::class);
+        $this->settingsRepository = $this->createMock(SettingsRepository::class);
+        $this->fileService = $this->createMock(FileService::class);
+
+        $this->settingsFacade = new SettingsFacade();
+
+        $this->settingsFacade->setFileService($this->fileService);
+        $this->settingsFacade->setTaskRepository($this->taskRepository);
+        $this->settingsFacade->setStudentRepository($this->studentRepository);
+        $this->settingsFacade->setSettingsRepository($this->settingsRepository);
+    }
+
     public function testLoadTasks()
     {
+        $this->fileService
+            ->method('loadInputFile')
+            ->willReturn([
+                ['Predmet','Nazev tematu','Osobni cislo','Jmeno','Nazev souboru','Pokus','Datum odevzdani','Autom. validace-Vysledek','Validator'],
+                ['KIV/OKS','OKS-01 - oks-01.jar','A10B0001P','RŮŽOVÁ Anita','oks-01.jar','1','02.03.2021 16:35','Špatné výsledky','http://validator.zcu.cz'],
+                ['KIV/OKS','OKS-02 - oks-02.jar','A10B1111P','SIVÝ Charlie','oks-02.jar','1','09.03.2021 18:38','Jiná chyba','http://validator.zcu.cz'],
+                ['KIV/OKS','OKS-01 - oks-01.jar','A10B4321P','ŠEDIVÝ Emil','oks-01.jar','1','03.03.2021 22:42','Jiná chyba','http://validator.zcu.cz']
+            ]);
 
+        $this->studentRepository
+            ->method('getStudentSchoolNumberIdMap')
+            ->willReturn(
+                ['A10B0001P' => 1],
+                ['A10B8765P' => 2],
+                ['A10B1111P' => 3],
+                ['A10B4321P' => 4],
+                ['A10B9999P' => 5]
+            );
+
+
+        $this->assertEquals(true, $this->settingsFacade->loadTasks(''));
     }
 
     public function testLoadStudents()
     {
-
+        $this->fileService
+            ->method('loadInputFile')
+            ->willReturn([
+                ['osCislo', 'jmeno', 'prijmeni'],
+                ['A10B0001P', 'Anita', 'RŮŽOVÁ'],
+                ['A10B8765P', 'Cyril', 'ŽLUTÝ'],
+                ['A10B1111P', 'Charlie', 'SIVÝ']
+            ]);
+        
+        $this->assertEquals(true, $this->settingsFacade->loadStudents(''));
     }
 
     public function testGetInputFiles()
     {
+        $this->fileService
+            ->method('getInputFiles')
+            ->willReturn(['1', '2']);
 
+        $inputFiles = $this->settingsFacade->getInputFiles();
+
+        $this->assertEquals(2, sizeof($inputFiles));
     }
 }
--- tests/model/facade/SummaryFacadeTest.php
+++ tests/model/facade/SummaryFacadeTest.php
@@ -36,23 +36,148 @@ class SummaryFacadeTest extends TestCase
 
     public function testGetOverviewData()
     {
+        $this->taskRepository
+            ->method('getMaxTaskNumber')
+            ->willReturn(3);
 
+        $this->taskRepository
+            ->method('getOverviewData')
+            ->willReturn([
+                ['name' => 1, 'total' => 14, 'is_ok_attempts' => 2, 'is_not_ok_attempts' => 12, 'succ_rate' => 0.1429, 'students_success_count' => 2, 'student_try_count' => 3, 'avg_per_student' => 4.6667],
+                ['name' => 2, 'total' => 5, 'is_ok_attempts' => 4, 'is_not_ok_attempts' => 1, 'succ_rate' => 0.8000, 'students_success_count' => 3, 'student_try_count' => 3, 'avg_per_student' => 1.6667],
+                ['name' => 3, 'total' => 3, 'is_ok_attempts' => 2, 'is_not_ok_attempts' => 1, 'succ_rate' => 0.6667, 'students_success_count' => 2, 'student_try_count' => 3, 'avg_per_student' => 1.0000],
+            ]);
+
+        $this->taskRepository
+            ->method('getOverviewMaxStudentAttemptsPerTask')
+            ->willReturn(
+                [1 => 10, 2 => 3, 3 => 1]
+            );
+
+        $this->taskRepository
+            ->method('getOverviewDataTotal')
+            ->willReturn(
+                ['name' => 1, 'total' => 22, 'is_ok_attempts' => 8, 'is_not_ok_attempts' => 14, 'succ_rate' => 0.3636, 'student_try_count' => 3]
+            );
+
+        $data = $this->summaryFacade->getOverviewData();
+
+        $this->assertEquals(2, sizeof($data));
+        $this->assertEquals(3, sizeof($data['task']));
+        $this->assertEquals($data['total']['studentsTryCount'], sizeof($data['task']));
+
+        $total = 0;
+        $ok = 0;
+        $notOk = 0;
+
+        foreach ($data['task'] as $task) {
+
+            $ok += $task['isOkAttempts'];
+            $notOk += $task['isNotOkAttempts'];
+            $total += $task['total'];
+        }
+
+        $this->assertEquals($data['total']['total'], $total);
+        $this->assertEquals($data['total']['isOkAttempts'], $ok);
+        $this->assertEquals($data['total']['isNotOkAttempts'], $notOk);
     }
 
     public function testGetStudentTableData()
     {
+        $this->taskRepository
+            ->method('getStudentTasksSummary')
+            ->willReturn([
+                ['name' => 1, 'total' => 2, 'end_date' => '2021-03-02 16:37:00', 'start_date' => '2021-03-02 16:35:00', 'unsuccessful_attempt' => 1, 'successful_attempt' => 1, 'success_rate' => 0.5],
+                ['name' => 2, 'total' => 1, 'end_date' => '2021-03-05 19:11:00', 'start_date' => '2021-03-05 19:11:00', 'unsuccessful_attempt' => 0, 'successful_attempt' => 1, 'success_rate' => 1],
+            ]);
 
+        $this->taskRepository
+            ->method('getStudentTasksTotalSummary')
+            ->willReturn([
+                ['total' => 3, 'end_date' => '2021-03-05 19:11:00', 'start_date' => '2021-03-02 16:35:00', 'unsuccessful_attempt' => 1, 'successful_attempt' => 2, 'success_rate' => 0.6667],
+            ]);
+
+        $data = $this->summaryFacade->getStudentTableData(0);
+
+        $this->assertEquals(2, sizeof($data));
+        $this->assertEquals(2, sizeof($data['student']));
+
+        $total = 0;
+        $ok = 0;
+        $notOk = 0;
+
+        foreach ($data['student'] as $task) {
+
+            $total += $task['total'];
+            $ok += $task['successfulAttempt'];
+            $notOk += $task['unsuccessfulAttempt'];
+        }
+
+        $this->assertEquals($data['total']['total'], $total);
+        $this->assertEquals($data['total']['successfulAttempt'], $ok);
+        $this->assertEquals($data['total']['unsuccessfulAttempt'], $notOk);
     }
 
     public function testGetGraphDataForStudent()
     {
+        $this->taskRepository
+            ->method('getStudentAttemptsTimes')
+            ->willReturn([
+                ['hour' => 16, 'count' => 2],
+                ['hour' => 19, 'count' => 1]
+            ]);
 
+        $data = $this->summaryFacade->getGraphDataForStudent(0);
+
+        $expected = [
+            0 => 0,
+            1 => 0,
+            2 => 0,
+            3 => 0,
+            4 => 0,
+            5 => 0,
+            6 => 0,
+            7 => 0,
+            8 => 0,
+            9 => 0,
+            10 => 0,
+            11 => 0,
+            12 => 0,
+            13 => 0,
+            14 => 0,
+            15 => 0,
+            16 => 2,
+            17 => 0,
+            18 => 0,
+            19 => 1,
+            20 => 0,
+            21 => 0,
+            22 => 0,
+            23 => 0
+        ];
+
+        $this->assertEquals($expected, $data);
     }
-
 
     public function testGetProgressBarData()
     {
+        $this->taskRepository
+            ->method('getProgressBarData')
+            ->willReturn([
+                ['unsuccessful' => 1, 'successful' => 0],
+                ['unsuccessful' => 0, 'successful' => 1],
+                ['unsuccessful' => 0, 'successful' => 1]
+            ]);
 
+        $this->settingsRepository
+            ->method('getTaskTotalCount')
+            ->willReturn(3);
+
+        $data = $this->summaryFacade->getProgressBarData(0);
+
+        $this->assertEquals(3, sizeof($data));
+        $this->assertEquals(1, $data['unsuccessful']);
+        $this->assertEquals(2, $data['successful']);
     }
 
     public function testGetGraphDataForOverview()
@@ -64,7 +189,7 @@ class SummaryFacadeTest extends TestCase
                 ['hour' => 5, 'count' => 10],
                 ['hour' => 23, 'count' => 23],
                 ['hour' => 8, 'count' => 7],
-                ['hour' => 0, 'count' => 20],
+                ['hour' => 0, 'count' => 20]
             ]);
 
         $data = $this->summaryFacade->getGraphDataForOverview();
